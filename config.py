0fae803b34f4cc713917d284a82f567c38bb9764
Parth Kathuria <pkathuria@nextracker.com>
2020-11-02 17:14:49 -0800

    updated constants.py

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 from pydantic import Field
 from pydantic.env_settings import BaseSettings
 
+from constants import DEFAULT_SOCRATA_DOMAIN, DEFAULT_SOCRATA_DATASET_ID, DEFAULT_PAGE_LIMIT
+
 
 class AppConfig(BaseSettings):
-    socrata_domain: str = Field(default="data.sfgov.org", env="SOCRATA_DOMAIN")
-    socrata_dataset_id: str = Field(default="jjew-r69b", env="SOCRATA_DATASET_ID")
+    socrata_domain: str = Field(default=DEFAULT_SOCRATA_DOMAIN, env="SOCRATA_DOMAIN")
+    socrata_dataset_id: str = Field(default=DEFAULT_SOCRATA_DATASET_ID, env="SOCRATA_DATASET_ID")
     app_token: str = Field(default=None, env="APP_TOKEN")
-    page_limit: int = Field(default=10, env="PAGE_LIMIT")
+    page_limit: int = Field(default=DEFAULT_PAGE_LIMIT, env="PAGE_LIMIT")
 
     class Config:
         env_file = "env/config.env"
--- constants.py
+++ constants.py
@@ -1,15 +1,6 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_FILE_PATH = "config.json"
-
-# config file keys
-SOCRATA_DOMAIN = "socrataDomain"
-SOCRATA_DATASET_ID = "socrataDatasetId"
-APP_TOKEN = "appToken"
-QUERY_LIMIT = "queryLimit"
+ENV_FILE_PATH = "env/config.env"
 
 # default values
 DEFAULT_SOCRATA_DOMAIN = "data.sfgov.org"
 DEFAULT_SOCRATA_DATASET_ID = "jjew-r69b"
-DEFAULT_QUERY_LIMIT = 10
+DEFAULT_PAGE_LIMIT = 10
